7c1ed0c38e43a901615f033552844720ab073dd7
Stefanmartin1967 <stefan.martin.1967@gmail.com>
2026-03-06 15:47:55 +0100

    Update base path for History-Walk to V2

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,13 @@ import { resolve } from 'path'; // 1. Ajout de l'import pour gérer les chemins
 
 export default defineConfig({
   // Le nom exact de votre dépôt GitHub
-  base: '/History-Walk-V1/',
+  base: '/History-Walk-V2/',
 
   plugins: [
     VitePWA({
       registerType: 'autoUpdate',
-      base: '/History-Walk-V1/',
-      scope: '/History-Walk-V1/',
+      base: '/History-Walk-V2/',
+      scope: '/History-Walk-V2/',
 
       manifest: false, // On utilise public/manifest.json manuellement
       workbox: {
